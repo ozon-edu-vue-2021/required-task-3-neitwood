b923dbd8ddb9adc28e8ff8bb97c2985504c06485
neitalex <neitwood@gmail.com>
2021-11-02 08:38:14 +0300

    added tables into group

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,23 +4,67 @@
 
     <div v-if="!isLoading" class="map-root">
       <!-- map -->
-      <MapSvg />
+      <MapSvg ref="svg" />
+      <WorkPlaceSvg v-show="false" ref="table" />
     </div>
     <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import tables from "../assets/data/tables.json";
 import MapSvg from "../assets/images/map.svg";
+import WorkPlaceSvg from "../assets/images/workPlace.svg";
 
 export default {
   data() {
     return {
       isLoading: false,
+      svg: null,
+      g: null,
+      tables: tables,
+      tableSVG: null, // добавляем шаблон стола
     };
   },
   components: {
     MapSvg,
+    WorkPlaceSvg,
+  },
+  mounted() {
+    this.isLoading = true;
+    this.svg = d3.select(this.$refs.svg);
+    this.g = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table); // получаем шаблон стола
+    if (this.g) {
+      this.drawTables();
+    } else {
+      alert("SVG is incorrect");
+    }
+    this.isLoading = false;
+  },
+  methods: {
+    drawTables() {
+      // создаем группу для рабочих мест
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+      this.tables.map((table) => {
+        // создает группу для рабочего стола
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+        // устанавливает стол в группу
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html());
+      });
+    },
   },
 };
 </script>
